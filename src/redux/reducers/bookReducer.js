87c0c2da34bb29689fdd3d0081157547ecd67d2a
OndraFilip <zoomblick@gmail.com>
2019-04-17 14:01:53 +0200

    Basic card layout

--- src/redux/reducers/bookReducer.js
+++ src/redux/reducers/bookReducer.js
@@ -1,20 +1,32 @@
-import {ADD_BOOKS} from "../actionTypes";
+import { ADD_BOOKS } from "../actionTypes";
 
 const initialState = {
-    books:  []
+  books: [],
+  bookPreviews: [
+    { id: 1, name: "Bible", author: "Irena Csaladiova" },
+    { id: 2, name: "Bible", author: "Irena Csaladiova" },
+    { id: 3, name: "Bible", author: "Irena Csaladiova" },
+    { id: 4, name: "Bible", author: "Irena Csaladiova" },
+    { id: 5, name: "Bible", author: "Irena Csaladiova" },
+    { id: 6, name: "Bible", author: "Irena Csaladiova" },
+    { id: 7, name: "Bible", author: "Irena Csaladiova" },
+    { id: 8, name: "Bible", author: "Irena Csaladiova" },
+    { id: 9, name: "Bible", author: "Irena Csaladiova" },
+    { id: 10, name: "Calculus", author: "Michael Spivak" }
+  ]
 };
 
 function bookReducer(state = initialState, action) {
-    console.log(action);
-    switch (action.type) {
-        case ADD_BOOKS:
-            return Object.assign({}, state, {
-                ...state,
-                books: state.books.concat(action.payload)
-            });
-        default:
-            return state
-    }
+  console.log(action);
+  switch (action.type) {
+    case ADD_BOOKS:
+      return Object.assign({}, state, {
+        ...state,
+        books: state.books.concat(action.payload)
+      });
+    default:
+      return state;
+  }
 }
 
 export default bookReducer;
