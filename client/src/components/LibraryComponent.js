9db6a042ae87be1182a6e178ac689492d8ba2197
Martin Biroščák <martin.biroscak@gmail.com>
2019-06-05 19:00:32 +0200

    Fix hover

--- client/src/components/LibraryComponent.js
+++ client/src/components/LibraryComponent.js
@@ -1,90 +1,90 @@
 import React from "react";
-import PageBookReader from "./PageBookReader";
-import { connect } from "react-redux";
-import {
-  Grid,
-  Card,
-  Image,
-  Dimmer,
-  Header,
-  Button,
-  Icon
-} from "semantic-ui-react";
+import {Button, Card, Dimmer, Grid, Icon, Image} from "semantic-ui-react";
 import {fetchMyBookPreviews} from "../redux/bookActionCreators";
+import {connect} from "react-redux";
 
 const mapStateToProps = state => {
-  return {
-    bookPreviews: state.bookReducer.bookPreviews
-  };
+    return {
+        bookPreviews: state.bookReducer.bookPreviews
+    };
 };
 
 
-
 const mapDispatchToProps = dispatch => ({
-  fetchMyBookPreviews: (user) => dispatch(fetchMyBookPreviews(user))
+    fetchMyBookPreviews: (user) => dispatch(fetchMyBookPreviews(user))
 });
 
-class LibraryComponent extends React.Component {
-  state = {};
+class BookCard extends React.Component {
 
-  componentDidMount() {
-    this.props.fetchMyBookPreviews()
-  }
+    state = {};
 
-  handleShow = () => this.setState({ active: true });
-  handleHide = () => this.setState({ active: false });
+    handleShow = () => this.setState({active: true});
 
-  render() {
-    const columns = this.props.bookPreviews.map(bookPreview => {
-      const { active } = this.state;
-      const content = (
-        <div>
-          <Button inverted animated size="huge">
-            <Button.Content visible>Read</Button.Content>
-            <Button.Content hidden>
-              <Icon name="align left" />
-            </Button.Content>
-          </Button>
-          <Button inverted animated size="huge">
-            <Button.Content visible>Info</Button.Content>
-            <Button.Content hidden>
-              <Icon name="info circle" />
-            </Button.Content>
-          </Button>
-        </div>
-      );
-      return (
-        <Grid.Column key={bookPreview.id}>
-          <Card>
-            <Dimmer.Dimmable
-              as={Image}
-              blurring
-              dimmed={active}
-              dimmer={{ active, content }}
-              onMouseEnter={this.handleShow}
-              onMouseLeave={this.handleHide}
-              // size="medium"
-              src={"data:image/" + bookPreview.coverExtension + ";base64," + bookPreview.coverData}
-            >
-              {/*</Dimmer.Dimmable>/<Image src="https://react.semantic-ui.com/images/avatar/large/matthew.png" />*/}
-            </Dimmer.Dimmable>
-            <Card.Content>
-              <Card.Header>{bookPreview.name}</Card.Header>
-              <Card.Meta>
-                <span>{bookPreview.author}</span>
-              </Card.Meta>
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-      );
-    });
+    handleHide = () => this.setState({active: false});
 
-    return (
-      <Grid doubling columns={6}>
-        {columns}
-      </Grid>
-    );
-  }
+    render() {
+        const bookPreview = this.props.bookPreview
+        console.log(this.props)
+        const {active} = this.state;
+        const content = (
+            <div>
+                <Button inverted animated size="huge">
+                    <Button.Content visible>Read</Button.Content>
+                    <Button.Content hidden>
+                        <Icon name="align left"/>
+                    </Button.Content>
+                </Button>
+                <Button inverted animated size="huge">
+                    <Button.Content visible>Info</Button.Content>
+                    <Button.Content hidden>
+                        <Icon name="info circle"/>
+                    </Button.Content>
+                </Button>
+            </div>
+        );
+        return (
+            <Grid.Column key={bookPreview.id}>
+                <Card>
+                    <Dimmer.Dimmable
+                        as={Image}
+                        blurring
+                        dimmed={active}
+                        dimmer={{active, content}}
+                        onMouseEnter={this.handleShow}
+                        onMouseLeave={this.handleHide}
+                        // size="medium"
+                        src={"data:image/" + bookPreview.coverExtension + ";base64," + bookPreview.coverData}
+                    >
+                        {/*</Dimmer.Dimmable>/<Image src="https://react.semantic-ui.com/images/avatar/large/matthew.png" />*/}
+                    </Dimmer.Dimmable>
+                    <Card.Content>
+                        <Card.Header>{bookPreview.name}</Card.Header>
+                        <Card.Meta>
+                            <span>{bookPreview.author}</span>
+                        </Card.Meta>
+                    </Card.Content>
+                </Card>
+            </Grid.Column>
+        );
+    }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(LibraryComponent);
+class LibraryComponent extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchMyBookPreviews()
+    }
+
+    render() {
+        const columns = this.props.bookPreviews.map((bookPreview) => <BookCard
+    bookPreview={bookPreview}/>)
+        return <Grid doubling columns={6}>
+            {columns}
+        </Grid>
+
+    }
+}
+
+
+ export default connect(mapStateToProps, mapDispatchToProps)(LibraryComponent)
+
